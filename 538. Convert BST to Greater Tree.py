a4ad9e08afe91de88001a974291f5a47ba914d43
VamsiMohanRamineedi <vmr286@nyu.edu>
2019-02-06 16:30:22 -0500

    Add initial solution to leetcode 538

---
+++ 538. Convert BST to Greater Tree.py
@@ -0,0 +1,38 @@
+# Convert BST to Greater Tree: Time: O(n), space: O(log n) average case and O(n) worst case
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def convertBST(self, root: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return root
+        self.sum = 0 # to maintain sum of the nodes on the right side
+        
+        def helper(node):
+            
+            # leaf node, sum will be sum till then plus leaf node value
+            if not node.left and not node.right: 
+                self.sum += node.val
+                node.val = self.sum
+                return self.sum
+            
+            # always traverse to the right most node in the tree, which is the largest value in BST
+            if node.right:
+                node.val += helper(node.right)
+                self.sum = node.val
+                    
+            if not node.right:
+                node.val += self.sum
+            
+            if node.left:
+                self.sum = node.val
+                return helper(node.left)
+            return self.sum
+        helper(root)
+        return root
+                
