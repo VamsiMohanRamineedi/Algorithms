9e52048afc772283efac44380ea0e38042333d2a
VamsiMohanRamineedi <vmr286@nyu.edu>
2019-03-31 16:35:15 -0400

    Initial time and space complexities as per my understanding, have to update them if required

--- 230. Kth smallest element in a bst.py
+++ 230. Kth smallest element in a bst.py
@@ -1,4 +1,4 @@
-# Kth smallest element in a BST
+# Kth smallest element in a BST: Time - O(n), space: O(log n) average case, O(n) worst case
 class Solution(object):
     def kthSmallest(self, root, k):
         # Unlike inorder traversal method below which forms an array of ALL nodes, we cut off the search as soon as kth smallest is found.
