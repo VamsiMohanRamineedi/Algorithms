298092e9c5debe2efde3a4f36dc37819128bf08f
VamsiMohanRamineedi <vmr286@nyu.edu>
2019-03-31 16:33:37 -0400

    Add solution to leetcode 230, update change in time/space complexity if any

---
+++ 230. Kth smallest element in a bst.py
@@ -0,0 +1,33 @@
+# Kth smallest element in a BST
+class Solution(object):
+    def kthSmallest(self, root, k):
+        # Unlike inorder traversal method below which forms an array of ALL nodes, we cut off the search as soon as kth smallest is found.
+        stack = []
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k = k-1
+            if k==0:
+                return root.val
+            root = root.right
+        
+'''
+class Solution(object):
+    def kthSmallest(self, root, k):
+        if not root:
+            return root
+        visited = []
+        
+        def inOrderTraverse(node):
+            if node.left:
+                inOrderTraverse(node.left)
+            visited.append(node.val)
+            if node.right:
+                inOrderTraverse(node.right)
+        
+        inOrderTraverse(root)
+        return visited[k-1]
+        
+'''
