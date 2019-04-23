26c92593cbed7b34416405ddb547a51b151de5b5
VamsiMohanRamineedi <vmr286@nyu.edu>
2019-04-23 16:25:21 -0400

    Easier version - leetcode 1021

--- 1021. Remove outermost parentheses.py
+++ 1021. Remove outermost parentheses.py
@@ -1,30 +1,33 @@
 # Remove Outermost parentheses: Time and space: O(n)
 class Solution:
     def removeOuterParentheses(self, S: str) -> str:
-        output = ''
-        remove = False
-        primitive_stack = []
+        open = 0
+        result = ''
         
         for char in S:
-            if not remove: # skips '(' in each primitive
-                remove = True
-            elif char == '(':
-                output += char
-                primitive_stack.append(char)
-            elif primitive_stack:
-                output += char
-                primitive_stack.pop()
-            elif not primitive_stack: # skips ')'
-                remove = False
-        return output      
-                
-'''
-    def removeOuterParentheses(self, S):
-        res, opened = [], 0
-        for c in S:
-            if c == '(' and opened > 0: res.append(c)
-            if c == ')' and opened > 1: res.append(c)
-            opened += 1 if c == '(' else -1
-        return "".join(res)          
-'''
-            
+            if open > 0 and char == '(':
+                result += char
+            elif open > 1 and char == ')':
+                result += char
+            open += 1 if char == '(' else -1
+        return result
+
+# A bit complex solution, but same time and space complexity
+# class Solution:
+#     def removeOuterParentheses(self, S: str) -> str:
+#         output = ''
+#         remove = False
+#         primitive_stack = []
+        
+#         for char in S:
+#             if not remove: # skips '(' in each primitive
+#                 remove = True
+#             elif char == '(':
+#                 output += char
+#                 primitive_stack.append(char)
+#             elif primitive_stack:
+#                 output += char
+#                 primitive_stack.pop()
+#             elif not primitive_stack: # skips ')'
+#                 remove = False
+#         return output      
