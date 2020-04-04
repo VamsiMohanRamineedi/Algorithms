f47ce088445a4901dd5d6606d64a7e53ef25a23d
VamsiMohanRamineedi <vmr286@nyu.edu>
2020-04-04 15:08:08 -0400

    Count of Unique Values in a list - python

---
+++ countUniqueValues.py
@@ -0,0 +1,22 @@
+# Time: O(n), space: O(n)
+
+def countUniqueValues(lst):
+	res = []
+	if len(lst) == 0:
+		return res
+	if len(lst) >= 1:
+		res.append(lst[0])
+
+	first = 0
+	second = 1
+	while(second < len(lst)):
+		if lst[first] != lst[second]:
+			res.append(lst[second])
+			first = second
+			second = first + 1
+		else:
+			second += 1
+	return res
+
+
+print(countUniqueValues([1,1,3,5,5,5,9]))
