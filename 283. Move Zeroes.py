e071a4df82a622e361b9729b746bd7863f8ac6e3
VamsiMohanRamineedi <vmr286@nyu.edu>
2020-07-22 19:55:20 -0400

    Reduce the replace operations in Q 283 leetcode

--- 283. Move Zeroes.py
+++ 283. Move Zeroes.py
@@ -1,16 +1,29 @@
 # 283. Move Zeroes - Time: O(n), space: O(1)
 class Solution:
-    def moveZeroes(self, nums):
+    def moveZeroes(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: void Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-        insertNonZeroElemIndex = 0
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[insertNonZeroElemIndex], nums[i] = nums[i],nums[insertNonZeroElemIndex]
-                insertNonZeroElemIndex += 1
-        return
+        rep_idx = 0
+        for i, num in enumerate(nums):
+            if num != 0:
+                if rep_idx != i:
+                    nums[rep_idx], nums[i] = nums[i], nums[rep_idx]
+                rep_idx += 1
+
+
+# class Solution:
+#     def moveZeroes(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: void Do not return anything, modify nums in-place instead.
+#         """
+#         insertNonZeroElemIndex = 0
+#         for i in range(len(nums)):
+#             if nums[i] != 0:
+#                 nums[insertNonZeroElemIndex], nums[i] = nums[i],nums[insertNonZeroElemIndex]
+#                 insertNonZeroElemIndex += 1
+#         return
                  
     
 
